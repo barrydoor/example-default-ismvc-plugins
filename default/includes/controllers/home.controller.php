564779a30a0d54097bf5c748f8c421130589dee5
Ivan <ivan.nederland@gmail.com>
2014-06-03 15:51:21 +0200

    Update home.controller.php

--- default/includes/controllers/home.controller.php
+++ default/includes/controllers/home.controller.php
@@ -5,15 +5,12 @@
 		$title='Home';
 		$data_theme='b';
 		$data_dividertheme='a'; 
-		 		
 		$content = HomeModel::database();
 		
 		render('home',array(
-		 
 			'title'				=> $title,
 			'data_theme'			=> $data_theme, 
 			'data_dividertheme'		=> $data_dividertheme,
-			
 			'content'			=> $content
 		));
 	}
