f444a416a43f47d9bbf45e4535d933ef08ea5efd
Ivan <ivan.nederland@gmail.com>
2014-06-03 15:52:43 +0200

    Update itemdelete.controller.php

--- default/includes/controllers/itemdelete.controller.php
+++ default/includes/controllers/itemdelete.controller.php
@@ -1,14 +1,13 @@
 <?php
  class ItemDeleteController{
 	public function handleRequest(){ 
-	
 		$title='Item verwijderen';
 		$data_theme='a';
 		$data_dividertheme='b'; 
+		ItemDeleteModel::delete($_POST['id']);  
 		
-	    ItemDeleteModel::delete($_POST['id']);  
  		render('itemdelete',array( 
-			'title'					=> $title,
+			'title'				=> $title,
 			'data_theme'			=> $data_theme, 
 			'data_dividertheme'		=> $data_dividertheme 
 		));
