6f68290aa4c1a1bda0521b236bb73211c56051b3
Ivan <ivan.nederland@gmail.com>
2014-06-03 15:51:59 +0200

    Update item.controller.php

--- default/includes/controllers/item.controller.php
+++ default/includes/controllers/item.controller.php
@@ -1,18 +1,16 @@
 <?php
  class ItemController{
 	public function handleRequest(){
-		 
 		$title='Item';
 		$data_theme='a';
 		$data_dividertheme='b'; 
-		 	
 		$content = ItemModel::find($_GET[id]);
 		
 		render('item',array( 
-			'title'					=> $title,
+			'title'				=> $title,
 			'data_theme'			=> $data_theme, 
 			'data_dividertheme'		=> $data_dividertheme,
-	 		'content'				=> $content
+	 		'content'			=> $content
 		));
 	}
 }
