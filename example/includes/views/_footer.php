8132c001c57cadbebc1c153489212bc5347aa641
Ivan <ivan.nederland@gmail.com>
2014-06-03 14:45:39 +0200

    Create _footer.php

---
+++ example/includes/views/_footer.php
@@ -0,0 +1,11 @@
+   <div data-role="footer"  data-theme="a"  data-dividertheme="a"  data-position="fixed">
+		    <div data-role="navbar">
+			<ul>  
+				<li><a href="./#" >Plugin Example</a> </li> 
+			</ul>
+	       </div>
+   </div>
+</div>
+			 
+</body>
+</html>
