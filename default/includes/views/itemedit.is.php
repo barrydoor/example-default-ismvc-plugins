a6391f622903b49759549b240b348440ace51de5
Ivan <ivan.nederland@gmail.com>
2014-06-03 16:12:06 +0200

    Create itemedit.is.php

---
+++ default/includes/views/itemedit.is.php
@@ -0,0 +1,8 @@
+ <li> 
+   <form action="./" method="post" data-mini="true" > 
+ <input title="is"     type="hidden"  name="is" value="item-update" />
+ <input title="id"      type="hidden"  name="id" value="<?php echo $itemeditmodel->id; ?>">
+ <input title="item"    name="item" value="<?php echo $itemeditmodel->item; ?>">
+ <input type="submit"   data-theme="b" data-mini="true"  data-icon="check" data-inline="true"  value="Wijzigen" /> 
+</form>       
+ </li>
