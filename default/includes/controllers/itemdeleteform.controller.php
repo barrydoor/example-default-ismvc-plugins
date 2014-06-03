b524f9436d2e542254c0d1e51b40eb786ede2129
Ivan <ivan.nederland@gmail.com>
2014-06-03 15:53:08 +0200

    Update itemdeleteform.controller.php

--- default/includes/controllers/itemdeleteform.controller.php
+++ default/includes/controllers/itemdeleteform.controller.php
@@ -1,13 +1,12 @@
 <?php
  class ItemDeleteFormController{
 	public function handleRequest(){
-		 
 		$title='Item verwijderen - Form';
 		$data_theme='a';
 		$data_dividertheme='b'; 
 		
  		render('itemdelete.form',array( 
-			'title'					=> $title,
+			'title'				=> $title,
 			'data_theme'			=> $data_theme, 
 			'data_dividertheme'		=> $data_dividertheme 
 		));
