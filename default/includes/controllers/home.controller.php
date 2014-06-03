a0a6512521506e747356ed45b5fec9ee9f20bb3a
Ivan <ivan.nederland@gmail.com>
2014-06-03 15:36:08 +0200

    Update home.controller.php

--- default/includes/controllers/home.controller.php
+++ default/includes/controllers/home.controller.php
@@ -1,23 +1,20 @@
 <?php
  class HomeController{
 	public function handleRequest(){
-		
-// Verplichte vars
+		 
 		$title='Home';
 		$data_theme='b';
 		$data_dividertheme='a'; 
-		
-// Uit de database		
+		 		
 		$content = HomeModel::database();
 		
 		render('home',array(
-			//vaste vars
-			'title'					=> $title,
+		 
+			'title'				=> $title,
 			'data_theme'			=> $data_theme, 
 			'data_dividertheme'		=> $data_dividertheme,
 			
-			//uit de database
-			'content'				=> $content
+			'content'			=> $content
 		));
 	}
 }
