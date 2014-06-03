1786ff5012dbb71ec7ffeaec08c6a06a037e7f19
Ivan <ivan.nederland@gmail.com>
2014-06-03 16:03:23 +0200

    Create _footer.php

---
+++ default/includes/views/_footer.php
@@ -0,0 +1,12 @@
+    	</div>        
+	   <div data-role="footer"  data-theme="a"  data-dividertheme="d"  data-position="fixed">
+		    <div data-role="navbar">
+			<ul> 
+				<li><a href="./?is=home" >Home</a></li> 
+				<li><a href="./?is=item-toevoegen" >Insert</a></li>
+			</ul>
+	       </div>
+	   </div>  
+</div> 
+</body>
+</html>
