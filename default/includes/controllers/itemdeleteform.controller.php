5d2d0400bcc5f0d030289a46c922df84295b08cb
Ivan <ivan.nederland@gmail.com>
2014-06-03 15:47:56 +0200

    Create itemdeleteform.controller.php

---
+++ default/includes/controllers/itemdeleteform.controller.php
@@ -0,0 +1,16 @@
+<?php
+ class ItemDeleteFormController{
+	public function handleRequest(){
+		 
+		$title='Item verwijderen - Form';
+		$data_theme='a';
+		$data_dividertheme='b'; 
+		
+ 		render('itemdelete.form',array( 
+			'title'					=> $title,
+			'data_theme'			=> $data_theme, 
+			'data_dividertheme'		=> $data_dividertheme 
+		));
+	}
+}
+ 
