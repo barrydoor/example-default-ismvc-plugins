2c7db5db04a8824ccea7788857919d1ce615376f
Ivan <ivan.nederland@gmail.com>
2014-06-03 16:08:07 +0200

    Create item.is.php

---
+++ default/includes/views/item.is.php
@@ -0,0 +1,3 @@
+<li> 
+   <a href="?edit=<?php echo $itemmodel->id; ?>"><?php echo $itemmodel->item; ?></a> <a  data-icon="delete" href="?delete-form=<?php echo $itemmodel->id; ?>">Delete</a>
+</li>
