2600c5bb4618cd106457e108310d58cab4acb67a
Ivan <ivan.nederland@gmail.com>
2014-06-03 16:21:02 +0200

    Update index.php

--- default/index.php
+++ default/index.php
@@ -1,16 +1,15 @@
 <?php
 require_once "includes/autoloading.php"; 
 require_once "includes/development.php"; 
-
 	try {
 	    if($get){ 
-			$controller = new $get();
+		$controller = new $get();
 		}
-	    else if($post){ 
+	    	else if($post){ 
 			$controller = new $post();
-		}	  
+			}	  
  		else throw new Exception('Wrong page!');
-			$controller->handleRequest();
+		$controller->handleRequest();
 		}
  	catch(Exception $e) { 
 		render('error',array('message'=>$e->getMessage()));
