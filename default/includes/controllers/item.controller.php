b324fd59e4e6a93ff540567de93a8382e2c931a3
Ivan <ivan.nederland@gmail.com>
2014-06-03 15:37:05 +0200

    Create item.controller.php

---
+++ default/includes/controllers/item.controller.php
@@ -0,0 +1,18 @@
+<?php
+ class ItemController{
+	public function handleRequest(){
+		 
+		$title='Item';
+		$data_theme='a';
+		$data_dividertheme='b'; 
+		 	
+		$content = ItemModel::find($_GET[id]);
+		
+		render('item',array( 
+			'title'					=> $title,
+			'data_theme'			=> $data_theme, 
+			'data_dividertheme'		=> $data_dividertheme,
+	 		'content'				=> $content
+		));
+	}
+}
