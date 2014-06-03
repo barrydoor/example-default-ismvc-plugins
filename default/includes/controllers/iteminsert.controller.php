ec40f60fa733013394fe46b584e0beaf893b909d
Ivan <ivan.nederland@gmail.com>
2014-06-03 15:49:30 +0200

    Create iteminsert.controller.php

---
+++ default/includes/controllers/iteminsert.controller.php
@@ -0,0 +1,23 @@
+<?php
+ class ItemInsertController{
+	public function handleRequest(){
+		 
+		$title='Item toevoegen';
+		$data_theme='a';
+		$data_dividertheme='b'; 
+		 
+	  if($_POST['item']){
+	   	 ItemInsertModel::insert($_POST['item']);  
+		 $message= 'Succesvol verlopen!';
+		 }
+		 else{
+			 $message= 'Fout! Het tekstveld mag niet leeg zijn!';
+		 }  
+		render('iteminsert',array( 
+			'title'					=> $title,
+			'data_theme'			=> $data_theme, 
+			'data_dividertheme'		=> $data_dividertheme,
+			'message'				=> $message  
+		));
+	}
+}
