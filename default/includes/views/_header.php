c64651cc422a0d120e119eaa92bafa30335ea612
Ivan <ivan.nederland@gmail.com>
2014-06-03 16:05:01 +0200

    Create _header.php

---
+++ default/includes/views/_header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html> 
+<html> 
+	<head> 
+	<title><?php echo formatTitle($title)?></title> 
+<meta name="viewport" content="width=device-width, initial-scale=1" /> 
+ 
+<link rel="stylesheet" href="http://code.jquery.com/mobile/latest/jquery.mobile.min.css" /> 
+<script src="http://code.jquery.com/jquery-git2.min.js"></script>
+<script src="http://code.jquery.com/mobile/latest/jquery.mobile.min.js"></script>
+    <link rel="stylesheet" href="assets/css/styles.css" />  
+</head> 
+<body> 
+
+<div data-role="page">
+	<div data-role="header"  data-theme="a" data-position="fixed">
+       <h1><?php echo $title?></h1>
+	</div> 
+	<div data-role="content">
