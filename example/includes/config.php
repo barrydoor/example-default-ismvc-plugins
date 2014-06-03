efa2350dccaece4ab26cdf08b4d58b36c846d370
Ivan <ivan.nederland@gmail.com>
2014-06-03 14:49:31 +0200

    Create config.php

---
+++ example/includes/config.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+
+$db_host = 'localhost';
+$db_user = 'db_user';
+$db_pass = 'db_pass';
+$db_name = 'db_name';
+
+$defaultTitle = 'Example';
+$defaultFooter = date('Y').' &copy; is-MVC';
