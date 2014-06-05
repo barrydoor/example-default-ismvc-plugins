0bb8d966be55596a29e3145eda56b7256225092c
Ivan <ivan.nederland@gmail.com>
2014-06-05 14:59:39 +0200

    Create home.is.php

---
+++ bloeddruk/includes/views/home.is.php
@@ -0,0 +1,3 @@
+ <li>
+<a href="./" > <?php echo $homemodel->hoog; ?> / <?php echo $homemodel->laag; ?>&nbsp;&nbsp; <span class=klein> <em><?php echo $homemodel->puls; ?></em> </span>  </a> <span class="ui-li-count"><?php echo $homemodel->tijd; ?></span>   <a href="./?delete=<?php echo $homemodel->id_rel; ?>" >Verwijderen</a>
+ </li> 
