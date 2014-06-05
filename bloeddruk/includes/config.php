d8f8a6814111c9d856df1ceef49d603cbd8ad062
Ivan <ivan.nederland@gmail.com>
2014-06-05 15:02:30 +0200

    Create config.php

---
+++ bloeddruk/includes/config.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+$db_host = 'localhost';
+$db_user = 'user';
+$db_pass = 'pass';
+$db_name = 'name';
+$defaultTitle = 'Bloeddruk';
+$defaultFooter = date('Y').' &copy; Barrydoor';
