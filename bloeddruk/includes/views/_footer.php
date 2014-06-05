020097b25090e51775e8bb04c6150fe50b912b42
Ivan <ivan.nederland@gmail.com>
2014-06-05 14:58:09 +0200

    Create _footer.php

---
+++ bloeddruk/includes/views/_footer.php
@@ -0,0 +1,16 @@
+    	</div>
+ 
+        
+	   <div data-role="footer"  data-theme="a"  data-dividertheme="d"  data-position="fixed">
+		    <div data-role="navbar">
+			<ul> 
+				<li><a href="./?is=overzicht" >Overzicht</a></li> 
+				<li><a href="./?is=metingen-toevoegen" >Metingen toevoegen</a></li> 
+			</ul>
+	       </div>
+	   </div> 
+       
+</div>
+
+</body>
+</html>
