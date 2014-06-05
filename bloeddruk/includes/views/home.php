aaba739ad0e503fb243a7d8e8d15c513bffa9659
Ivan <ivan.nederland@gmail.com>
2014-06-05 14:59:59 +0200

    Create home.php

---
+++ bloeddruk/includes/views/home.php
@@ -0,0 +1,8 @@
+<?php render('_header',array('title'=>$title))?>			 
+ 	<ul data-role="listview"   data-theme="d" data-dividertheme="d">
+   		<li data-role="list-divider">SYS / DIA [ PULSE ]</li>
+    </ul> 
+	<ul data-role="listview" data-inset="true" data-theme="d" data-divider-theme="b"  data-split-icon="delete">  
+  <?php render($content) ?>
+ </ul> 
+<?php render('_footer')?>
